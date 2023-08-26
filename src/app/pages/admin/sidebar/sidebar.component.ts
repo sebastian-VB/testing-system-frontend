fbeecec2c4b33dc40e9d4e175e3bc6b2d1c12a09
sebastian-VB <sebas99peru@gmail.com>
2023-08-26 13:26:21 -0500

    added latest funcionalities

--- src/app/pages/admin/sidebar/sidebar.component.ts
+++ src/app/pages/admin/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+
+  constructor(private loginSvc: LoginService, private router: Router){}
+
+  logout(): void{
+    this.loginSvc.logout();
+    this.router.navigate(['login']);
+  }
+
 
 }
